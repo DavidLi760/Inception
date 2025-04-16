584d689e551d3476250724d2a675a7e14a57859d
davli <davli@Inception.davli.42.fr>
2025-04-16 16:18:30 +0200

    s

---
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ ! -f "/var/www/html/wp-config.php" ]; then
+
+	cd /var/www/html
+	wp core download --allow-root
+
+	until mysqladmin --user=${MYSQL_USER} --password=${MYSQL_PASSWORD} --host=mariadb ping; do
+		sleep 2
+	done
+
+	wp config create	--dbname=${MYSQL_DATABASE} \
+						--dbuser=${MYSQL_USER} \
+						--dbpass=${MYSQL_PASSWORD} \
+						--dbhost=mariadb \
+						--allow-root
+	wp core install		--url=${DOMAIN_NAME} \
+						--title=${WP_TITLE} \
+						--admin_user=${WP_ADMIN} \
+						--admin_password=${WP_ADMIN_PASSWORD} \
+						--admin_email=${WP_ADMIN_EMAIL} \
+						--skip-email \
+						--allow-root
+	wp user create 		${WP_USER} ${WP_USER_EMAIL} \
+						--user_pass=${WP_USER_PASSWORD} \
+						--role=author \
+						--allow-root
+
+fi;
+
+exec "$@"
